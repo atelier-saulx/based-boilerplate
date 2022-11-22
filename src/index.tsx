bf56f76f95ecb14e8973e0d9bfb021882aa2c023
Nuno Frade <nfrade.tribal@gmail.com>
2022-11-22 14:25:13 +0100

    Added basic boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render } from 'react-dom'
+// import based from '@based/client'
+
+// Don't forget to edit your based.json file with your project info
+//
+// import basedConfig from '../based.json'
+// export const client = based(basedConfig)
+
+const App = () => {
+  return <div>Hello world</div>
+}
+
+render(<App />, document.body)
