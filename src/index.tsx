e1daa352f9b2f39fd6e041fbb80f9cc6798c5769
Nuno Frade <nfrade.tribal@gmail.com>
2022-11-22 17:43:02 +0100

    Styles hello world

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,37 @@ import { render } from 'react-dom'
 // export const client = based(basedConfig)
 
 const App = () => {
-  return <div>Hello world</div>
+  return (
+    <div
+      style={{
+        background: '#131313',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <img
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+        src="https://user-images.githubusercontent.com/683825/203369546-5b50d2f8-71cc-4d13-a7a1-f9a67f2072f6.svg"
+      />
+      <h2
+        style={{
+          color: '#bbbbbb',
+          paddingTop: '60px',
+          fontFamily: 'sans-serif',
+          fontWeight: 800,
+          fontSize: '20pt',
+        }}
+      >
+        Hello World!
+      </h2>
+    </div>
+  )
 }
 
 render(<App />, document.body)
