ade5b0b13ea90a5d3a06d00183386326fd7e9853
kylemarch513 <kyle.march513@gmail.com>
2023-11-20 14:30:07 +0100

    boiler plate

---
+++ app/Login/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { styled } from '@based/ui'
+
+export const Login = () => {
+  return <styled.div style={{}}>asdfasdfasdf</styled.div>
+}
--- app/app.tsx
+++ app/app.tsx
@@ -1,43 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './globals.css'
 import based from '@based/client'
 import { Provider, useQuery } from '@based/react'
+import { Avatar, TopNavigation, color, styled } from '@based/ui'
+import { Login } from './Login'
 
 import basedConfig from '../based.json'
 export const client = based(basedConfig)
 
 const App = () => {
   const { data: counter, loading } = useQuery('counter')
+
+  return <Login />
+
   return (
-    <div
+    <styled.div
       style={{
-        background: '#131313',
-        height: '100vh',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
+        backgroundColor: color('background', 'default'),
+
+        paddingTop: 50,
       }}
     >
-      <img
-        style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-        src="https://user-images.githubusercontent.com/683825/203369546-5b50d2f8-71cc-4d13-a7a1-f9a67f2072f6.svg"
-      />
-      <h2
-        style={{
-          color: '#bbbbbb',
-          paddingTop: '60px',
-          fontFamily: 'sans-serif',
-          fontWeight: 800,
-          fontSize: '20pt',
-        }}
-      >
-        Hello World! {loading ? '-' : counter}
-      </h2>
-    </div>
+      <TopNavigation>
+        <Avatar>asdasd</Avatar>
+      </TopNavigation>
+    </styled.div>
   )
 }
 
