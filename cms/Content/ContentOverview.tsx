fcbed5b7144f3f5464994f2f8f119e0726e07603
yves <misterybeer@gmail.com>
2023-12-01 17:57:15 +0100

    foe

--- cms/Content/ContentOverview.tsx
+++ cms/Content/ContentOverview.tsx
@@ -12,7 +12,19 @@ export const ContentOverview = () => {
   const route = useRoute('[section][id]')
   const routeSection = route.query.section
 
-  console.log('👽', schema?.types[routeSection as string].fields)
+  console.log('👽', schema?.types[routeSection as string]?.fields)
+
+  // TODO: ordering columns based on index
+  // order these fields by meta order
+  // if (schema) {
+  //   console.log(Object.values(schema?.types[routeSection as string].fields))
+  //   let fields = Object.values(schema?.types[routeSection as string].fields)
+
+  //   let sortedByMetaIndex = fields.sort((p1, p2) =>
+  //     p2.meta.index < p1.meta.index ? 1 : p2.meta.index > p1.meta.index ? -1 : 0
+  //   )
+  //   console.log('in right order?', sortedByMetaIndex)
+  // }
 
   return (
     <styled.div style={{ padding: '24px 48px', width: '100%' }}>
@@ -32,6 +44,8 @@ export const ContentOverview = () => {
         <Button
           icon={<IconPlus />}
           size="small"
+          color="primary"
+          ghost
           onClick={async () => {
             await client.call('db:set', {
               // TODO check this ? en default?
