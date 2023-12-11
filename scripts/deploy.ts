e962dfb67703b51561b6eb51a5b99340e29d3741
yves <misterybeer@gmail.com>
2023-12-11 12:08:01 +0100

    recovering some mistakies

--- scripts/deploy.ts
+++
@@ -1,8 +0,0 @@
-// websitehook is for website branch and preview sites only
-//
-const mainhook =
-  'https://api.cloudflare.com/client/v4/pages/webhooks/deploy_hooks/5a101718-05f5-4113-b554-5a4efb78a096'
-
-fetch(mainhook, {
-  method: 'POST',
-})
